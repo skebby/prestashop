581b830e678ec90a0afb280cc2ba7ba067d20f55
mattimatti <mmonti@gmail.com>
2015-07-27 11:25:45 +0200

    Added dynamic date

--- testshipmentnotification.php
+++ testshipmentnotification.php
@@ -34,13 +34,16 @@ if (Tools::getValue('token') != Tools::encrypt(Configuration::get('PS_SHOP_NAME'
 
 $skebby_module = new Skebby();
 
+
+$date=date_create();
+
 $params = array();
 $params['customer_mobile'] = Configuration::get('SKEBBY_DEFAULT_NUMBER');
 $params['civility'] = 'Mr.';
-$params['first_name'] = 'Matteo';
-$params['last_name'] = 'Monti';
+$params['first_name'] = 'John';
+$params['last_name'] = 'Wayne';
 $params['order_price'] = 'EUR 10.15';
-$params['order_date'] = '2015-01-09 14:16:47';
+$params['order_date'] = date_format($date,"Y-m-d H:i:s");
 $params['order_reference'] = 'ABCDEFGHI';
 
 echo Tools::jsonEncode($skebby_module->sendMessageForOrder($params, 'SKEBBY_SHIPMENTSTATUS_NOTIFICATION_TEMPLATE'));
