6bb9c1cd3dfa24f16bf07d16cf829de394a7b1ea
mattimatti <mmonti@gmail.com>
2015-07-27 22:10:23 +0200

    ok

--- skebby.php
+++ skebby.php
@@ -1318,7 +1318,7 @@ class Skebby extends Module
 	public function getCustomers()
 	{
 	    $dbquery = new DbQuery();
-	    $dbquery->select('c.`id_customer` AS `id`, CONCAT(a.`phone_mobile`, p.`call_prefix`) AS `phone_mobile`,  p.`call_prefix` AS `call_prefix`, s.`name` AS `shop_name`, gl.`name` AS `gender`, c.`lastname`, c.`firstname`, c.`email`, c.`newsletter` AS `subscribed`, c.`newsletter_date_add`');
+	    $dbquery->select('c.`id_customer` AS `id`, CONCAT(`+`, CONCAT(p.`call_prefix`,a.`phone_mobile` )) AS `phone_mobile`, s.`name` AS `shop_name`, gl.`name` AS `gender`, c.`lastname`, c.`firstname`, c.`email`, c.`newsletter` AS `subscribed`, c.`newsletter_date_add`');
 	    $dbquery->from('customer', 'c');
 	    $dbquery->leftJoin('shop', 's', 's.id_shop = c.id_shop');
 	    $dbquery->leftJoin('gender', 'g', 'g.id_gender = c.id_gender');
